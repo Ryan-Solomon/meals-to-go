b5c8f50ec98dd4f771795934752e03b152104d65
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-29 15:56:37 -0600

    add layout for search and list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <SearchBar />
+      <List />
+    </Container>
   );
 }
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  padding: 20px 0px;
+`;
+
+const SearchBar = styled.View`
+  background: teal;
+  height: 10%;
+  width: 100%;
+`;
+
+const List = styled.View`
+  background: blue;
+  height: 90%;
+  width: 100%;
+`;
