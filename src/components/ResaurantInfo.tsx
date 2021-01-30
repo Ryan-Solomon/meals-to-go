86e93c761a73d7c85ad6406753c79f418b60dcad
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-30 16:39:23 -0600

    add open icon

--- src/components/ResaurantInfo.tsx
+++ src/components/ResaurantInfo.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import styled from 'styled-components/native';
 import { SvgXml } from 'react-native-svg';
 import star from '../../assets/star';
+import open from '../../assets/open';
 import nextId from 'react-id-generator';
 
 export type TRestaurant = {
@@ -30,7 +31,8 @@ export const RestaurantInfo: FC<Props> = ({
     isClosedTemporarily,
   },
 }) => {
-  const stars = new Array(rating).fill(null);
+  const stars = new Array(Math.floor(rating)).fill(null);
+
   return (
     <Container style={{ elevation: 10 }}>
       {/* @ts-ignore */}
@@ -38,9 +40,14 @@ export const RestaurantInfo: FC<Props> = ({
       <ContentContainer>
         <Title>{name}</Title>
         <StarsContainer>
-          {stars.map((_) => (
-            <SvgXml xml={star} key={nextId()} height={20} width={20} />
-          ))}
+          <View style={{ flexDirection: 'row' }}>
+            {stars.map((_) => (
+              <SvgXml xml={star} key={nextId()} height={20} width={20} />
+            ))}
+          </View>
+          <View>
+            {isOpenNow && <SvgXml xml={open} height={30} width={30} />}
+          </View>
         </StarsContainer>
         <SubTitle>{address}</SubTitle>
       </ContentContainer>
@@ -51,14 +58,14 @@ export const RestaurantInfo: FC<Props> = ({
 const styles = StyleSheet.create({
   image: {
     width: '100%',
-    height: '80%',
+    height: 200,
     resizeMode: 'cover',
   },
 });
 
 const Container = styled.View`
   width: 100%;
-  height: 450px;
+  /* height: 450px; */
   padding: ${({ theme }) => theme.space[3]};
   background-color: white;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
@@ -71,6 +78,7 @@ const ContentContainer = styled.View`
 const Title = styled.Text`
   font-size: ${({ theme }) => theme.fontSizes.h4};
   font-family: ${({ theme }) => theme.fonts.heading};
+  margin-top: ${({ theme }) => theme.space[2]};
 `;
 const SubTitle = styled.Text`
   font-size: ${({ theme }) => theme.fontSizes.h6};
@@ -79,4 +87,8 @@ const SubTitle = styled.Text`
 
 const StarsContainer = styled.View`
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+  padding-right: 10px;
 `;
