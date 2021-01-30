1bd459999c8244c35a958134df3942ca45156ab2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-30 16:27:11 -0600

    add rating with star svg

--- src/components/ResaurantInfo.tsx
+++ src/components/ResaurantInfo.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 import { Image, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
-import { theme } from './../infrastructure/theme/index';
+import { SvgXml } from 'react-native-svg';
+import star from '../../assets/star';
+import nextId from 'react-id-generator';
 
 export type TRestaurant = {
   name: string;
@@ -28,12 +30,18 @@ export const RestaurantInfo: FC<Props> = ({
     isClosedTemporarily,
   },
 }) => {
+  const stars = new Array(rating).fill(null);
   return (
     <Container style={{ elevation: 10 }}>
       {/* @ts-ignore */}
       <Image style={styles.image} source={photos[0]} />
       <ContentContainer>
         <Title>{name}</Title>
+        <StarsContainer>
+          {stars.map((_) => (
+            <SvgXml xml={star} key={nextId()} height={20} width={20} />
+          ))}
+        </StarsContainer>
         <SubTitle>{address}</SubTitle>
       </ContentContainer>
     </Container>
@@ -50,7 +58,7 @@ const styles = StyleSheet.create({
 
 const Container = styled.View`
   width: 100%;
-  height: 400px;
+  height: 450px;
   padding: ${({ theme }) => theme.space[3]};
   background-color: white;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
@@ -67,4 +75,8 @@ const Title = styled.Text`
 const SubTitle = styled.Text`
   font-size: ${({ theme }) => theme.fontSizes.h6};
   font-family: ${({ theme }) => theme.fonts.body};
+`;
+
+const StarsContainer = styled.View`
+  flex-direction: row;
 `;
