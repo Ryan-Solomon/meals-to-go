a80024532bb350c2f36b8bd853260e2dcd7b52a2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-31 14:17:08 -0600

    setup hook

--- src/hooks/useRestaurantData.tsx
+++ src/hooks/useRestaurantData.tsx
@@ -1,3 +1,10 @@
 // Create a custom hook to use SWR to fetch resaurant data
 // At first, just grab data from mock data
 // Then, you can actually use SWR once ya'll get there
+
+import React, { useState } from 'react';
+
+export const useRestaurantData = () => {
+  const [restaurant, setRestaurants] = useState([]);
+  return null;
+};
