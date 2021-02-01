9de1ed396aa313c423198c46dd36beaa1bad62a9
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-01 15:58:27 -0600

    start setting up stack nav

--- src/infrastructure/navigation/restaurantsNavigator.tsx
+++ src/infrastructure/navigation/restaurantsNavigator.tsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RestaurantsScreen } from './../../screens/RestaurantsScreen';
+import { DetailsScreen } from '../../screens/DetailsScreen';
+
+type RootStackParamList = {
+  Restaurants: undefined;
+  DetailsScreen: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const RestaurantNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Restaurants' component={RestaurantsScreen} />
+      <Stack.Screen name='DetailsScreen' component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
