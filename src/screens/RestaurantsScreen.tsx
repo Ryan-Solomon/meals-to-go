bdda0c437079f2048ff0b3c155943e7ed4477b15
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-04 17:30:19 -0600

    remove sbar component and add to restaurant screen

--- src/screens/RestaurantsScreen.tsx
+++ src/screens/RestaurantsScreen.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { RestaurantInfo, TRestaurant } from '../components/ResaurantInfo';
 import { FlatList, Text } from 'react-native';
 import { useRestaurantData } from './../hooks/useRestaurantData';
+import { Searchbar } from 'react-native-paper';
+import { useLocationContext } from '../context/location';
 
 export const RestaurantsScreen = () => {
+  const { location, setLocation } = useLocationContext();
   const { data, status } = useRestaurantData();
   if (status === 'loading') return <Text>Loading..</Text>;
   return (
     <>
+      {/* @ts-ignore */}
+      <Searchbar
+        placeholder='Search'
+        onChangeText={(t) => setLocation(t)}
+        value={location}
+        icon='magnify'
+      />
       <FlatList
         data={data}
         // @ts-ignore
