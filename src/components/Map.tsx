284424c0108a4587413f185b2ed59cb8e1201af2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-04 17:58:54 -0600

    setup useEffect to adjust latDelta

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,9 +3,23 @@ import MapView from 'react-native-maps';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 import { useLocationContext } from '../context/location';
+import { useRestaurantData } from '../hooks/useRestaurantData';
+import { Text } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export const Map = () => {
   const { location, setLocation } = useLocationContext();
+  const { data, status } = useRestaurantData();
+  const [latDelta, setLatDelta] = useState(0);
+
+  useEffect(() => {
+    const northeastLat = 41.88758823029149;
+    const southwestLat = 41.88489026970849;
+    const latDelta = northeastLat - southwestLat;
+    setLatDelta(latDelta);
+  }, [location]);
+
+  if (status === 'loading') return <Text>Loading..</Text>;
 
   return (
     <>
@@ -16,7 +30,11 @@ export const Map = () => {
         value={location}
         icon='map'
       />
-      <StyledMap />
+      <StyledMap>
+        {data.map((restaurant) => {
+          return null;
+        })}
+      </StyledMap>
     </>
   );
 };
