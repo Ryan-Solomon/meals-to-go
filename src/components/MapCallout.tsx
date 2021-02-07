2d42e2985b1dd88007e2ef14d7781bd6f373629d
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-07 07:31:34 -0600

    style MapCallout

--- src/components/MapCallout.tsx
+++ src/components/MapCallout.tsx
@@ -1,12 +1,31 @@
 import React, { FC } from 'react';
 import { TRestaurant } from './ResaurantInfo';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import styled from 'styled-components/native';
 
 type TProps = {
   restaurant: TRestaurant;
 };
 
 export const MapCallout: FC<TProps> = ({ restaurant }) => {
-  const { name } = restaurant;
-  return <Text>{name}</Text>;
+  const { name, photos } = restaurant;
+  return (
+    <Container>
+      {/* @ts-ignore */}
+      <SImage source={photos[0]} />
+      <SText>{name}</SText>
+    </Container>
+  );
 };
+
+const Container = styled.View`
+  background-color: #333;
+  width: 100px;
+  height: 100px;
+`;
+
+const SImage = styled(Image)``;
+
+const SText = styled.Text`
+  color: white;
+`;
