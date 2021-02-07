b8d3edc30da2c95f2dccc89ec03ac80de1815bca
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-07 10:11:58 -0600

    strongly type init favs context

--- src/context/favorites.tsx
+++ src/context/favorites.tsx
@@ -1,12 +1,39 @@
-import React, { createContext } from 'react';
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useState,
+} from 'react';
 import { TRestaurant } from '../components/ResaurantInfo';
 
 type TFavoritesContext = {
   favorites: TRestaurant[];
+  addFavorite: (restaurant: TRestaurant) => void;
+  removeFavorite: (id: string) => void;
+  clearFavorites: () => void;
 };
 
 const initialContext: TFavoritesContext = {
   favorites: [],
+  addFavorite: (restaurant: TRestaurant) => null,
+  removeFavorite: (id: string) => null,
+  clearFavorites: () => null,
 };
 
 const FavoritesContext = createContext(initialContext);
+
+export const FavoritesProvider: FC<ReactNode> = ({ children }) => {
+  const [favorites, setFavorites] = useState<TRestaurant[]>([]);
+
+  const value = {};
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export const useFavoritesContext = () => {
+  return useContext(FavoritesContext);
+};
