d1a13070e66ec6835ca212c803594258e764157f
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 16:28:54 -0600

    add account component

---
+++ src/components/Account.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Account = () => {
+  return (
+    <AccountContainer>
+      <AccountHeader>Meals To Go</AccountHeader>
+    </AccountContainer>
+  );
+};
+
+const AccountContainer = styled.View``;
+
+const AccountHeader = styled.Text``;
--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Account } from '../components/Account';
 
 export const AccountScreen = () => {
   return (
     <View>
-      <Text>Account</Text>
+      <Account />
     </View>
   );
 };
