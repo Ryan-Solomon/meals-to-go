54f3f3b94882d0db79377de4b7ed4a23a27a45a7
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 18:08:08 -0600

    add header to login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { BackgroundImage } from '../components/BackgroundImage';
+import styled from 'styled-components/native';
 const img = require('../../assets/bgLogin.jpg');
 
 export const LoginScreen = () => {
   return (
     <>
       <BackgroundImage image={img}>
-        <Text>Login</Text>
+        <BackgroundCover>
+          <Header>Login</Header>
+        </BackgroundCover>
       </BackgroundImage>
     </>
   );
 };
+
+const BackgroundCover = styled.View`
+  flex: 1;
+  background-color: rgba(0, 0, 0, 0.5);
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.Text`
+  color: #fff;
+  font-size: ${({ theme }) => theme.fontSizes.h4};
+`;
