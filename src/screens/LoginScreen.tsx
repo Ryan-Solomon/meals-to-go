f2e9360587bdfd9216fe05b39ff13706d0de348f
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 18:10:12 -0600

    init form

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -27,3 +27,21 @@ const Header = styled.Text`
   color: #fff;
   font-size: ${({ theme }) => theme.fontSizes.h4};
 `;
+
+const FormContainer = styled.View``;
+
+const Input = styled.TextInput``;
+
+const Button = styled.TouchableOpacity`
+  background-color: #333;
+  padding: ${({ theme }) => theme.space[3]};
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.fontSizes.h6};
+  text-align: center;
+`;
