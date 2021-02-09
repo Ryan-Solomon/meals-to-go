171dbce6f34cdeff4e21cec29fe9b72b9fb9089e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 18:03:52 -0600

    add loginScreen bg

--- src/infrastructure/navigation/accountNavigator.tsx
+++ src/infrastructure/navigation/accountNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AccountScreen } from '../../screens/AccountScreen';
 import { LoginScreen } from '../../screens/LoginScreen';
+import { RegisterScreen } from '../../screens/RegisterScreen';
 
 type TStackParamList = {
   Main: undefined;
@@ -16,7 +17,7 @@ export const AccountNavigator = () => {
     <Stack.Navigator headerMode='none'>
       <Stack.Screen name='Main' component={AccountScreen} />
       <Stack.Screen name='Login' component={LoginScreen} />
-      <Stack.Screen name='Register' component={LoginScreen} />
+      <Stack.Screen name='Register' component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { BackgroundImage } from '../components/BackgroundImage';
+const img = require('../../assets/bgLogin.jpg');
 
 export const LoginScreen = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <>
+      <BackgroundImage image={img}>
+        <Text>Login</Text>
+      </BackgroundImage>
+    </>
   );
 };
